c5fb25ccbcf259fe1c598fc4c307f46acd6c177a
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-03-27 22:53:44 +0600

    create a connection with mongodb

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const express=require('express')
 const cors=require('cors')
 const app=express()
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 // port 
 const port=process.env.PORT || 5000
 
 //middleware
 app.use(cors())
 app.use(express.json())
+
+// mongodb connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.25fgudl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 //run server
 app.get("/",(req,res)=>{
